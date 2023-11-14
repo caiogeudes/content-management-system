bde5b5061b7efd9a0673743e75c2e0a59de16e0f
Caio <caiogeudes@gmail.com>
2023-11-14 14:01:33 -0300

    feature - editar conteúdo

--- src/controllers/content.js
+++ src/controllers/content.js
@@ -29,7 +29,29 @@ const updateContent = async (req, res) => {
     }
 }
 
+const editContent = async (req, res) => {
+    const { title, content } = req.body;
+    const { id } = req.params;
+    if (title && content) {
+        return res.status(400).json({ mensagem: 'Neste endpoint, só podemos editar um campo por vez. Escolha apenas "Title" ou "Content" para editá-lo.' });
+    } else {
+        try {
+            if (title) {
+                const editedContent = await knex('content').update({ title }).where('id', id).returning('*');
+                return res.status(200).json(editedContent[0]);
+            } else if (content) {
+                const editedContent = await knex('content').update({ content }).where('id', id);
+                return res.status(200).json(editedContent[0]).returning('*');
+            }
+        } catch (error) {
+            console.log(error.message);
+            return res.status(500).json({ mensagem: 'Erro interno do servidor.' })
+        }
+    }
+}
+
 module.exports = {
     uploadContent,
-    updateContent
+    updateContent,
+    editContent
 }
--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const { createUser } = require('./controllers/user');
 const { login } = require('./controllers/login');
 const { validateNewUser, validateLogin, validateContent } = require('./middlewares/validations');
 const { verificateLoggedUser } = require('./middlewares/verifications');
-const { uploadContent, updateContent } = require('./controllers/content');
+const { uploadContent, updateContent, editContent } = require('./controllers/content');
 const rota = express();
 
 rota.post('/user', validateNewUser, createUser);
@@ -11,5 +11,6 @@ rota.post('/login', validateLogin, login);
 rota.use(verificateLoggedUser);
 rota.post('/content', validateContent, uploadContent);
 rota.put('/content/:id', validateContent, updateContent);
+rota.patch('/content/:id', validateContent, editContent);
 
 module.exports = rota;
